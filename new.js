89011040b2716b18f06150d1b3e7893859aff3e5
Niklas-Schaefers <Niklas.Schaefers@gmx.de>
2021-04-28 14:05:45 +0200

    Created storage

--- new.js
+++ new.js
@@ -12,14 +12,44 @@
 // submitTaskInput.onclick = handleClick;
 
 // const submitTaskInput = document.querySelector(".addtaskButton");
-const taskNameInput = document.querySelector(".add-new");
+
+// formElement.onsubmit = function (event) {
+//   event.preventDefault();
+//   const taskNameInput = document.querySelector(".add-new");
+//   const checkedDateInput = document.querySelector(
+//     ".radioContainer__input:checked"
+//   );
+//   if (checkedDateInput !== null && !taskNameInput.value) {
+//     alert(taskNameInput.value + " " + checkedDateInput.value);
+//   } else {
+//     alert("Ups, please fill in task and select date");
+//   }
+// };
+
 const formElement = document.querySelector(".formNewPage");
 
 formElement.onsubmit = function (event) {
+  //Prevent the default form submit behavior (sending data to a server an reloading page)
   event.preventDefault();
+
+  const taskNameInput = document.querySelector(".add-new");
   const checkedDateInput = document.querySelector(
     ".radioContainer__input:checked"
   );
 
-  console.log(taskNameInput.value, checkedDateInput.value);
+  if (!taskNameInput.value) {
+    console.log("Text input is empty");
+    return;
+  } else if (!checkedDateInput) {
+    console.log("Select date is empty");
+    return;
+  } else {
+    const task = {
+      taskName: taskNameInput.value,
+      dateInput: checkedDateInput.value,
+    };
+
+    const taskJSON = JSON.stringify(task);
+    localStorage.setItem("task", taskJSON);
+  }
 };
