af867db6249e93607415166a9a32b09be5644e32
Niklas-Schaefers <Niklas.Schaefers@gmx.de>
2021-04-29 17:30:22 +0200

    Second Step (map) done

--- dashboard.js
+++ dashboard.js
@@ -13,9 +13,31 @@ function createTaskElement(taskName) {
   return label;
 }
 
-const heatTeeElement = createTaskElement("Tee kochen");
-const drinkTeeElement = createTaskElement("Tee trinken");
+function parseJSONFromLocalStorage(key, defaultValue) {
+  const json = localStorage.getItem(key);
+  if (json === null) {
+    return defaultValue;
+  }
+  const data = JSON.parse(json);
+  return data;
+}
 
+// Get Array with task Objects from localStorage
+const taskList = parseJSONFromLocalStorage("taskList", []);
+
+// Create taskElements array consisting of html elements base on the taskList
+const taskElements = taskList.map(function (task) {
+  return createTaskElement(task.name);
+});
+
+// Get parent element of tasks
 const tasksGroupElement = document.querySelector(".checkbox-group");
 
-tasksGroupElement.append(heatTeeElement, drinkTeeElement);
+// Append all elements in taskElement to task group
+tasksGroupElement.append(...taskListElements);
+
+// taskList[0].name
+
+// const heatTeeElement = createTaskElement("Tee kochen");
+// const drinkTeeElement = createTaskElement("Tee trinken");
+// tasksGroupElement.append(heatTeeElement, drinkTeeElement);
