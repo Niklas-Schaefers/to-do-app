2c9df44a696ad42a908864b45f3a3e71df79b4a1
Niklas-Schaefers <Niklas.Schaefers@gmx.de>
2021-04-29 14:42:48 +0200

    Load dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,21 @@
+// function createTaskElement(taskName) {
+//   const label = document.createElement("label");
+//   const input = document.createElement("input");
+//   const span = document.createElement("span");
+
+//   input.type = "checkbox";
+//   input.className = "task-group__input";
+
+//   span.className = "task-group__task";
+//   span.innerText = taskName;
+
+//   label.append(input, span);
+//   return label;
+// }
+
+// const heatTeeElement = createTaskElement("Tee kochen");
+// const drinkTeeElement = createTaskElement("Tee trinken");
+
+// const tasksGroupElement = document.querySelector(".tasks-group")
+
+// tasksGroupElement.append(heatTeeElement, drinkTeeElement)
